0a2998978c9143705428bdac7f7ce1ad5bd82c51
InadyaQ <fedoseevanada24@gmail.com>
2024-11-08 14:01:55 +0200

    New class 'Inventory' contains main action methods: - AddProduct: Adds a product to the inventory. - RemoveProduct: Removes a product from the inventory by its ID. - FindProduct: Finds a product by its ID. - DisplayAllProducts: Displays all products in the inventory.

---
+++ ProductManager/Inventory.cs
@@ -0,0 +1,45 @@
+﻿namespace ProductManager;
+
+public class Inventory
+{
+    private List<IProduct> items = new List<IProduct>();
+    public void AddProduct(IProduct product)
+    {
+        items.Add(product);
+        Console.WriteLine($"Product {product.Name} added to inventory");
+    }
+
+    public bool RemoveProduct(int id)
+    {
+        var item = FindProduct(id);
+        if (item != null)
+        {
+            items.Remove(item);
+            Console.WriteLine($"Product {item.Name} removed from inventory.");
+            return true;
+        }
+        Console.WriteLine("Product not found.");
+        return false;
+    }
+
+    public IProduct? FindProduct(int id)
+    {
+        return items.FirstOrDefault(i => i.Id == id);
+    }
+
+    public void DisplayAllProducts()
+    {
+        if (items.Any())
+        {
+            foreach (var product in items)
+            {
+                product.DisplayInfo();
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("No products in the inventory.");
+        }
+    }
+}
